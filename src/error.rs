8023946c853d619dd387a114668b9e1642808f50
Petr Beneš <w.benny@outlook.com>
2025-02-26 01:13:48 +0000

    refactor: remove unnecessary lifetime specifier from fmt method parameters

--- src/error.rs
+++ src/error.rs
@@ -24,7 +24,7 @@ pub struct XcError {
 impl std::error::Error for XcError {}
 
 impl std::fmt::Display for XcError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         write!(f, "{}", self.desc)
     }
 }
