1b19c3491ee00f0fc5fcefbee75a1e3e84905c0a
Petr Beneš <w.benny@outlook.com>
2026-04-29 13:33:25 +0000

    chore: allow "unnecessary_transmutes"
    This lint was introduced in Rust v1.88, so bump the MSRV, too.

--- crates/xen-sys/src/lib.rs
+++ crates/xen-sys/src/lib.rs
@@ -2,15 +2,7 @@
     non_camel_case_types,
     non_snake_case,
     non_upper_case_globals,
-    clippy::missing_safety_doc,
-    clippy::too_many_arguments,
-    clippy::useless_transmute,
-
-    // remove when a new version of bindgen is released
-    // https://github.com/rust-lang/rust-bindgen/pull/3124
-    unsafe_code,
-    unsafe_op_in_unsafe_fn,
-    clippy::ptr_offset_with_cast,
+    unnecessary_transmutes,
 )]
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
